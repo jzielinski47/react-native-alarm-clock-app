91dc05cb6286985e095379bea228d293d80c6db8
Jakub Zieliński <jzielinski47dev@gmail.com>
2022-12-11 22:07:21 +0100

    style-update

--- screens/Main.jsx
+++ screens/Main.jsx
@@ -1,19 +1,25 @@
 import { StyleSheet, Text, View, StatusBar, ActivityIndicator, TouchableOpacity, ImageBackground, Image, Dimensions } from 'react-native';
 import CustomButton from '../components/CustomButton';
+import { useFonts } from 'expo-font';
 import React from 'react'
 
 const screen = { width: Dimensions.get("window").width, height: Dimensions.get("window").height }
 
 const Main = ({ navigation }) => {
+
+    const [fontLoaded] = useFonts({ opensansRegular: require('../assets/fonts/OpenSans-Regular.ttf'), opensansBold: require('../assets/fonts/OpenSans-Bold.ttf'), })
+
+    if (!fontLoaded) { return null }
+
     return (
         <>
             <View style={theme.container}>
                 <Text style={[theme.lettermark, { top: 70 }]}>SQLITE APP</Text>
                 <Text style={[theme.lettermark, { bottom: 85 }]}>by Jakub Zielinski</Text>
                 <View style={theme.content} >
-                    <Image style={theme.illustration} source={{ uri: 'https://i.imgur.com/DbIYmzl.png' }} />
-                    <Text style={theme.title}>Alarm Clock App</Text>
-                    <Text style={theme.text}>start managing your time{"\n"}with alarm clocks</Text>
+                    <Image style={theme.illustration} source={require('../assets/illustration.png')} />
+                    <Text style={[theme.title, { fontFamily: 'opensansBold' }]}>Alarm Clock App</Text>
+                    <Text style={[theme.text, { fontFamily: 'opensansRegular' }]}>manage your time with alarm clocks</Text>
                     <CustomButton title={'Get started'} onPress={() => navigation.navigate('list')} />
                 </View>
                 <StatusBar style="auto" />
@@ -26,7 +32,7 @@ const theme = StyleSheet.create({
     container: { flex: 1, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' },
     lettermark: { fontSize: 12, color: 'gray', fontWeight: 'bold', position: 'absolute' },
     content: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-    illustration: { width: 200, height: 200 },
+    illustration: { width: 275, height: 150 },
     title: { fontSize: 32, textAlign: 'center' },
     text: { fontSize: 16, marginBottom: 30, textAlign: 'center' },
     colors: { primary: '#2673d0' }
