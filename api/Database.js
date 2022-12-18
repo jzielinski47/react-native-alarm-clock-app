63e4f0b7dd596a0c2be94739a16e064099e17b77
Jakub Zieliński <jzielinski47dev@gmail.com>
2022-12-18 09:53:55 +0100

    database-functionalities-update

--- api/Database.js
+++ api/Database.js
@@ -1,12 +1,17 @@
 import * as SQLite from "expo-sqlite";
 const db = SQLite.openDatabase("zielinski_jakub_4i1b.db");
 
+const tableName = 'table'
+
 export class Database {
 
-    static createTable = () => db.transaction(tx => tx.executeSql("CREATE TABLE IF NOT EXISTS table1 (id integer primary key not null, a text, b text);"));
-    static add = () => db.transaction(tx => tx.executeSql("INSERT INTO table1 (a, b) values ('xxx', 'yyy')"));
+    static createTable = () => db.transaction(tx =>
+        tx.executeSql(`CREATE TABLE IF NOT EXISTS ${tableName} (id integer primary key not null, hour text, minute text, active INTEGER);`));
+
+    static add = (hour, minute, active) => db.transaction(tx => tx.executeSql(`INSERT INTO ${tableName} (hour, minute, active) values (${hour},${minute},${active})`));
+
     static getAll = () => {
-        var query = "SELECT * FROM table1";
+        var query = `SELECT * FROM ${tableName}`;
 
         return new Promise((resolve, reject) => db.transaction((tx) => {
             tx.executeSql(query, [], (tx, results) => {
@@ -15,30 +20,17 @@ export class Database {
 
                 resolve(JSON.stringify(results));
 
-            }, function (tx, error) {
-
-                reject(error);
-
-            });
+            }, (tx, error) => reject(error));
         }))
 
     }
 
-    static remove() {
-        db.transaction(tx => {
-            tx.executeSql(
-                "DELETE FROM table1 WHERE (id = 0);"
-            );
-        });
-
-    }
+    static remove = (id) => db.transaction(tx => tx.executeSql(`DELETE FROM ${tableName} WHERE (id = ${id});`));
 
     static removeAll() {
 
         db.transaction(tx => {
-            tx.executeSql(
-                "DELETE FROM table1 ;"
-            );
+            tx.executeSql(`DELETE FROM ${tableName};`);
         });
     }
 
